1f98a499778576b7a5083e6b893207e98829f176
Jeff Brown <jeffjjbrown@gmail.com>
2020-05-19 18:02:52 -0700

    Initial commit for Planet Hopper

---
+++ Source/PlanetHopper/PlanetHopperGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlanetHopperGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLANETHOPPER_API APlanetHopperGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
